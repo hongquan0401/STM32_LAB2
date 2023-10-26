b5e25f58b313c916a8a964494216579f1f16ca16
DESKTOP-TO7CBI7\DELL F89F <hongquan040103@gmail.com>
2023-10-26 22:36:56 +0700

    init main with software timer Cycle 1ms

---
+++ Lab_2 main/Core/Src/softwareTimer.c
@@ -0,0 +1,25 @@
+/*
+ * softwareTimer.c
+ *
+ *  Created on: Oct 26, 2023
+ *      Author: DELL F89F
+ */
+
+#include "softwareTimer.h"
+
+int timer1_counter = 0;
+int timer1_flag = 0;
+
+void setTimer1(int duration) {
+	timer1_counter = duration;
+	timer1_flag = 0;
+}
+
+void timer1Run(){
+	if (timer1_counter > 0){
+		timer1_counter--;
+		if (timer1_counter <= 0){
+			timer1_flag = 1;
+		}
+	}
+}
